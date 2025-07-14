47862b2b355f3030e5824e0968369e146d94d977
Michal Zakrzewski <michal.zakrzewski@core.co.uk>
2025-07-14 18:49:13 +0200

    started writing html structure for website

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html/>
+<html class="no-js" lang="en" dir="ltr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>SimpleCal</title>
+        <link rel="stylesheet" href="css/style.css">
+        <meta name="description" content="A simple calendar for your simple needs">
+
+        <meta property="og:title" content="SimpleCal">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="">
+        <meta property="og:image" content="">
+        <meta property="og:image:alt" content="">
+
+        <link rel="icon" href="/favicon.ico" sizes="any">
+        <link rel="icon" href="/icon.svg" type="image/svg+xml">
+        <link rel="apple-touch-icon" href="icon.png">
+
+        <link rel="manifest" href="site.webmanifest">
+        <script src="https://kit.fontawesome.com/7d317bd9cc.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+
+    <header>
+        <h1>SimpleCal <i class="fa-regular fa-calendar-days fa-sm"></i><br></h1><h2>Your Simple Calendar</h2>
+    </header>
+
+    <!-- Clock -->
+    <div class="clock-container">
+        <div id="clock"></div>
+    </div>
+
+    <!-- cal section -->
+    <div class="calendar">
+        <div class="nav-btn-container">
+            <div class="nav-btn" onclick=""><i class="fa-solid fa-backward"></i></div>
+        </div>
+    </div>
+
+    </body>
+</html>
